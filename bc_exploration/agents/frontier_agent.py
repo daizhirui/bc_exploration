improve performance, correct exploration rate computation

bc_exploration/agents/frontier_agent.py
@@ -4,7 +4,6 @@ return a path to the "best" frontier.
 """
 from __future__ import print_function, absolute_import, division
 
-
 import cv2
 import numpy as np
 
@@ -17,9 +16,9 @@ from bc_exploration.planners.astar_cpp import oriented_astar, get_astar_angles
 from bc_exploration.utilities.util import wrap_angles, which_coords_in_bounds, xy_to_rc, rc_to_xy
 
 
-def extract_frontiers(occupancy_map, approx=True, approx_iters=2,
+def extract_frontiers(occupancy_map, approx=True, approx_iters=4,
                       kernel=cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3)),
-                      debug=False):
+                      debug=False, debug_min_frontier_size=None):
     """
     Given a map of free/occupied/unexplored pixels, identify the frontiers.
     This method is a quicker method than the brute force approach,
@@ -28,7 +27,7 @@ def extract_frontiers(occupancy_map, approx=True, approx_iters=2,
 
     :param occupancy_map Costmap: object corresponding to the map to extract frontiers from
     :param approx bool: does an approximation of the map before extracting frontiers. (dilate erode, get rid of single
-                   pixel unexplored areas creating a large number fo frontiers)
+                   pixel unexplored areas creating a large number of frontiers)
     :param approx_iters int: number of iterations for the dilate erode
     :param kernel array(N, N)[float]: the kernel of which to use to extract frontiers / approx map
     :param debug bool: show debug windows?
@@ -71,14 +70,20 @@ def extract_frontiers(occupancy_map, approx=True, approx_iters=2,
         frontier_map = np.repeat([occupancy_map.data], repeats=3, axis=0).transpose((1, 2, 0))
         # frontiers = [frontiers[rank] for i, rank in enumerate(frontier_ranks)
         #              if i < self.num_frontiers_considered]
+
+        cnt = 0
         for frontier in frontiers:
+            if debug_min_frontier_size and max([len(frontier), 1]) < debug_min_frontier_size:
+                continue
+            cnt += 1
             # if frontier.astype(int).tolist() in self.frontier_blacklist:
             #     continue
             frontier_px = xy_to_rc(frontier, occupancy_map).astype(int)
             frontier_px = frontier_px[which_coords_in_bounds(frontier_px, occupancy_map.get_shape())]
             frontier_map[frontier_px[:, 0], frontier_px[:, 1]] = [255, 0, 0]
 
-        plt.imshow(frontier_map, cmap='gray', interpolation='nearest')
+        plt.imshow(frontier_map, cmap='gray')
+        plt.title(f'{cnt} frontiers')
         plt.show()
 
     return frontiers

bc_exploration/envs/grid_world.py
@@ -50,7 +50,9 @@ class GridWorld:
 
         self._state = None
         self._map = None
-        self._truth_free_coords = None
+        # self._truth_free_coords = None
+        self._truth_free_mask = None
+        self._truth_free_area = None
 
         self._load_map(map_filename, map_resolution, map_padding)
 
@@ -119,12 +121,20 @@ class GridWorld:
         :param occupancy_map Costmap: input map to be compared with ground truth
         :return float: percentage covered of the input map to the ground truth map
         """
-        if self._truth_free_coords is None:
-            start_state_px = xy_to_rc(self._start_state, self._map)
-            self._truth_free_coords = compute_connected_pixels(start_state_px, self._map.data)
+        if self._truth_free_mask is None:
+            self._truth_free_mask = self._map.data == Costmap.FREE
+            self._truth_free_area = np.sum(self._truth_free_mask)
+        mask1 = occupancy_map.data == Costmap.FREE
+        # mask2 = self._map.data == Costmap.FREE
+        # mask2 = self._truth_free_mask
+        intersect = mask1 & self._truth_free_mask
+        return np.sum(intersect) / self._truth_free_area
 
-        # free_coords = np.argwhere(occupancy_map.data == Costmap.FREE)
-        return np.sum(occupancy_map.data == Costmap.FREE) / float(self._truth_free_coords.shape[0])
+        # if self._truth_free_mask is None:
+        #     start_state_px = xy_to_rc(self._start_state, self._map)
+        #     self._truth_free_mask = compute_connected_pixels(start_state_px, self._map.data)
+
+        # return np.sum(occupancy_map.data == Costmap.FREE) / float(self._truth_free_mask.shape[0])
 
     def step(self, desired_state):
         """

bc_exploration/utilities/util.py
@@ -138,7 +138,8 @@ def scan_to_points(angles, ranges):
     :param ranges array(N)[float]: ranges corresponding to the angles
     :return array(N,2)[float]: ndarray, points
     """
-    points = np.nan * np.ones((angles.shape[0], 2))
+    # points = np.nan * np.ones((angles.shape[0], 2))
+    points = np.full((angles.shape[0], 2), np.nan)
     good_inds = np.logical_not(np.logical_or(np.isinf(ranges), np.isnan(ranges)))
     points[good_inds, :] = np.expand_dims(ranges[good_inds], axis=1) * \
         np.array([np.cos(angles[good_inds]), np.sin(angles[good_inds])]).T